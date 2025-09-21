e0f19f2c40496bfb336bd8e0ba2034fe14be65b0
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-09-21 20:48:56 +0200

    Google sso btn done

--- jetPackPam/app/src/main/java/student/projects/jetpackpam/MainActivity.kt
+++ jetPackPam/app/src/main/java/student/projects/jetpackpam/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import student.projects.jetpackpam.screens.accounthandler.LoginScreen
+import student.projects.jetpackpam.screens.accounthandler.SignUpScreen
 import student.projects.jetpackpam.screens.mainapp.MainScreen
 import student.projects.jetpackpam.ui.theme.JetPackPamTheme
 
+
+// if you are lost look at the screen package and then the auth screens for log in
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +19,8 @@ class MainActivity : ComponentActivity() {
             JetPackPamTheme(){
 
                 //MainScreen()
-                LoginScreen()
+               // LoginScreen()
+                //SignUpScreen()
             }
         }
     }
