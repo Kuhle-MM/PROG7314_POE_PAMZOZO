c380878a25972dd364270caa3f88747c42a6e913
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-09-21 20:35:45 +0200

    GoogleCalendarController, events and service classes added

--- API/Services/GoogleCalendarService.cs
+++ API/Services/GoogleCalendarService.cs
@@ -1,6 +1,22 @@
-﻿namespace PROG7314_POE.Services
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+
+namespace PROG7314_POE.Services
 {
     public class GoogleCalendarService
     {
+        public static CalendarService GetCalendarServiceFromToken(string accessToken)
+        {
+            var credential = GoogleCredential.FromAccessToken(accessToken);
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "P.A.M - Personal Assistant with Mobility"
+            });
+
+            return service;
+        }
     }
 }
