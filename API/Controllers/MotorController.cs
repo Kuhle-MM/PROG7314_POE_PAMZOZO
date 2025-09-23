708ba09cfde324caabcb12ce7105333efea9c420
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-09-23 13:09:44 +0200

    Calculations for the speed and directions added for the camera and motor

--- API/Controllers/MotorController.cs
+++ API/Controllers/MotorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PROG7314_POE.Models;
 using PROG7314_POE.Services;
 
 namespace PROG7314_POE.Controllers
@@ -9,10 +10,31 @@ namespace PROG7314_POE.Controllers
     public class MotorController : ControllerBase
     {
         //POST method to send the co-ordinates to the raspberry pi for the motor directions
+        [HttpPost("moveMotors")]
+        public IActionResult MoveMotors([FromBody] Motor motorCommand)
+        {
+            // Convert joystick vector (x,y) to motor PWM
+            double left = (motorCommand.y) + (motorCommand.x);
+            double right = (motorCommand.y) - (motorCommand.x);
 
-        //POST method to send the co-ordinates to the raspberry pi for the motor direction
+            // Normalize to 0–100 range
+            int leftPwm = (int)(Math.Clamp(left, -1, 1) * 100);
+            int rightPwm = (int)(Math.Clamp(right, -1, 1) * 100);
 
+            var response = new Motor
+            {
+                leftMotorSpeed = leftPwm,
+                rightMotorSpeed = rightPwm
+            };
 
+            return Ok(response);
+        }
+
+        //POST method to send the co-ordinates to the raspberry pi for the motor speed
+        [HttpPost("stopMotors")]
+        public IActionResult StopMotors() //stops both wheels
+        {
+            return Ok(new Motor { leftMotorSpeed = 0, rightMotorSpeed = 0 });
+        }
     }
-
 }
--- API/Models/Motor.cs
+++ API/Models/Motor.cs
@@ -5,8 +5,8 @@
         //sending to the raspberry pi
         public double x { get; set; } //this is the x axis of the motor
         public double y { get; set; } //this is the y axis of the motor
-        public double leftMotorSpeed { get; set; }
-        public double rightMotorSpeed { get; set; }
+        public int leftMotorSpeed { get; set; } //0-100%
+        public int rightMotorSpeed { get; set; } //0-100%
 
     }
 }
