4b0f6dbc5590ec94a558a6a012648627e911f42d
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-09-23 12:22:42 +0200

    Camera and Motor classes added for control of camera and motor speed and direction

---
+++ API/Controllers/MotorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PROG7314_POE.Services;
+
+namespace PROG7314_POE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotorController : ControllerBase
+    {   
+
+    }
+
+    
+}
---
+++ API/Models/Motor.cs
@@ -0,0 +1,13 @@
+﻿namespace PROG7314_POE.Models
+{
+    public class Motor
+    {
+        //sending to the raspberry pi
+        public double x { get; set; } //this is the x axis of the motor
+        public double y { get; set; } //this is the y axis of the motor
+        public double leftMotorSpeed { get; set; }
+        public double rightMotorSpeed { get; set; }
+
+
+    }
+}
