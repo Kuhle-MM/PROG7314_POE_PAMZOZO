7c6ef64cbf4cd9a1fd69434a0f8ec6e4f55cbdb9
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-09-26 11:09:55 +0200

    PersonalityScreen 1 and 2 are complete. They show different options to display the personalities

--- jetPackPam/app/src/main/java/student/projects/jetpackpam/screens/firsttimecustom/PersonalityScreen2.kt
+++ jetPackPam/app/src/main/java/student/projects/jetpackpam/screens/firsttimecustom/PersonalityScreen2.kt
@@ -5,41 +5,49 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.items
 
 @Composable
-fun LanguageSelectionScreen() {
+fun PersonalitySelectionScreen2()
+{
     val context = LocalContext.current
-    val languages = listOf(
-        "English",
-        "Afrikaans",
-        "isiZulu"
+    val personalities = listOf(
+        "Sarcastic",
+        "Friendly",
+        "Gen Z",
+        "Never in the mood",
+        "Motivational Coach",
+        "Wise Elder",
+        "Cheerful Optimist",
+        "Storyteller",
+        "Shakespearean",
+        "Tech Geek",
     )
     Column(
         modifier = Modifier
@@ -48,49 +56,49 @@ fun LanguageSelectionScreen() {
         horizontalAlignment = Alignment.CenterHorizontally // center horizontally
     ) {
         // Top Heading
-        LanguageHeader()
-    }
+        PersonalityHeader2()
+
+    Spacer(modifier = Modifier.height(50.dp)) // gap between header and grid
     //Option carousel
-    LazyRow(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(22.dp),
-        verticalAlignment = Alignment.CenterVertically
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2), // 2 columns
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(26.dp),
+        modifier = Modifier.padding(20.dp)
     )
     {
-        items(languages) { language ->
+        items(personalities) { personalities ->
             OutlinedCard(
                 onClick = {
                     //sets whole app in selected language
-                    Toast.makeText(context, "Clicked on $language", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Clicked on $personalities", Toast.LENGTH_SHORT).show()
                 },
-                shape = CircleShape,
+                shape = RoundedCornerShape(30),
                 colors = CardDefaults.cardColors(
                     containerColor = Color.Transparent
                 ),
                 border = BorderStroke(7.dp, Color(0xFFF0A1F8)),
-                modifier = Modifier.size(250.dp)
+                modifier = Modifier.size(280.dp)
             ) {
                 Box(
                     modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        text = language,
+                        text = personalities,
                         textAlign = TextAlign.Center,
                         fontSize = 29.sp
                     )
                 }
             }
         }
-    }
+    }}
 }
 
 @Composable
-fun LanguageHeader(){
+fun PersonalityHeader2(){
     Text(
-        text = "Select preferred language",
+        text = "Choose my personality",
         fontStyle = FontStyle.Italic,
         fontSize = 35.sp
     )
