99813a74c5adc06054bf9c27e7cfea525df6bd07
hlumelontwanambi <st10383786@vcconnect.edu.za>
2025-09-29 12:22:42 +0200

    LangAPI

---
+++ API/Models/TranslationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PROG7314_POE.Models
+{
+    public class TranslationRequest
+    {
+        public string Text { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
