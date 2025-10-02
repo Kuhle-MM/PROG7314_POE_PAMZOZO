6f133fe7df2c277d9090e9dfad074a083306fafd
hlumelontwanambi <st10383786@vcconnect.edu.za>
2025-10-02 12:58:59 +0200

    TransAPI

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,9 @@ namespace PROG7314_POE
             builder.Services.AddScoped<GeminiService>();
             builder.Services.AddSingleton<NavigationService>();
 
+            builder.Services.AddHttpClient<TranslationService>();
+            builder.Services.AddScoped<TranslationService>();
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
