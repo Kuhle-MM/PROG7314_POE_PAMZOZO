8f89c8f144bcd75fab1ca3b1778c8e7ad0d7ad3b
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-10-06 10:47:32 +0200

    Charades categories are added and pulled through from the api. User can select which category to choose and game session ids.

--- jetPackPam/app/build.gradle.kts
+++ jetPackPam/app/build.gradle.kts
@@ -59,9 +59,14 @@ dependencies {
     implementation(libs.material3.adaptive)
     implementation(libs.androidx.ui.text.google.fonts)
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //API
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //Nav Graph
+    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.activity:activity-compose:1.8.2")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
---
+++ jetPackPam/app/src/main/java/student/projects/jetpackpam/retrofit/LanguageRetrofitInstance.kt
@@ -0,0 +1,11 @@
+package student.projects.jetpackpam.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val retrofit = Retrofit.Builder()
+    .baseUrl("https://pamapi-bdguhpdzbahca0f9.eastasia-01.azurewebsites.net/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+val languageApi = retrofit.create(LanguageApi::class.java)
