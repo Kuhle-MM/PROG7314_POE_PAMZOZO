3e763bf6514e72215b43cb23be1ef84fa4381936
Kuhle-MM <ST10259861@vcconnect.edu.za>
2025-10-13 08:00:33 +0200

    Movement and camera controls

--- jetPackPam/app/src/main/java/student/projects/jetpackpam/retrofit/PiRetrofitInstance.kt
+++ jetPackPam/app/src/main/java/student/projects/jetpackpam/retrofit/PiRetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object PiRetrofitInstance {
-    private const val BASE_URL = "http://192.168.101.206:7298/"
+    private const val BASE_URL = "http://192.168.137.1:7298/"
 
     val api: RobotApi by lazy {
         Retrofit.Builder()
